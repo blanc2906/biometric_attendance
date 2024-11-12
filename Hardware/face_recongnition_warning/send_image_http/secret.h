01d6537c3bb0f774be7959cda1c06736fa882a66
bvlang2k3 <buivietlang2k@gmail.com>
2024-11-12 16:32:37 +0700

    first

--- Hardware/face_recongnition_warning/send_image_http/secret.h
+++
@@ -1,7 +0,0 @@
-#include <pgmspace.h>
-
-#define SECRET
-
-const char WIFI_SSID[] = "Phong 304";
-const char WIFI_PASSWORD[] = "Buivietlang29062003";
-const char SERVER_URL[] = "http://your-server:5000/process-image";
